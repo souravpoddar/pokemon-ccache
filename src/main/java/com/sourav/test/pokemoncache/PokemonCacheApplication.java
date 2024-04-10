2312186d873c3e4ff50764149d7413809bd5d554
souravpoddar <souravpoddar1992@gmail.com>
2024-04-10 17:52:42 +0530

    Add files via upload

---
+++ src/main/java/com/sourav/test/pokemoncache/PokemonCacheApplication.java
@@ -0,0 +1,13 @@
+package com.sourav.test.pokemoncache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonCacheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokemonCacheApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sourav/test/pokemoncache/PokemonCacheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sourav.test.pokemoncache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonCacheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
